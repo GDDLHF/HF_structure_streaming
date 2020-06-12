cd9428cb442517b9dcb80c0b4b2071b65f72ac13
tom <tom@163.com>
2020-06-12 22:46:45 +0800

    第5次提交

--- src/main/scala/structure_streaming/Test/test4.scala
+++ src/main/scala/structure_streaming/Test/test4.scala
@@ -2,7 +2,7 @@ package src.main.scala.structure_streaming.Test
 
 object test4 {
   def main(args: Array[String]): Unit = {
-    //4444
+    //55555555555
 
 
   }
---
+++ src/main/scala/structure_streaming/Test/test5.scala
@@ -0,0 +1,5 @@
+package src.main.scala.structure_streaming.Test
+
+object test5 {
+
+}
